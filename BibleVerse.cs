7ed70d12f2fb0da860015c2febb218e0dc0709b5
SMSN-88 <samson.a@hotmail.co.uk>
2025-08-27 14:25:51 +0100

    Initial commit

---
+++ BibleVerse.cs
@@ -0,0 +1,12 @@
+﻿namespace BibleVerseGenApp
+{
+    public class BibleVerse
+    {
+        public int Pk { get; set; }
+        public string Translation { get; set; }
+        public int Book { get; set; }
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+        public string Text { get; set; }
+    }
+}
